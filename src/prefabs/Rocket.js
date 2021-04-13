683b879f0991375e709b5fb94966893cffe0c462
rynogolf <collinrowell00@gmail.com>
2021-04-12 17:27:54 -0700

    Final Commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,8 +5,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         // add object to the existing scene
         scene.add.existing(this);
-        this.isFiring = false;      // track rocket firing status
-        this.moveSpeed = 2;         // pixels per frame
+        this.isFiring = false;                              // track rocket firing status
+        this.moveSpeed = 2;                                 // pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket');     // add rocket sfx
     }
 
     update() {
@@ -19,11 +20,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
 
-        //if fired, move the rocekt up
+        //if fired, move the rocket up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
         }
